6b67ba9a1d6c8629f0f80877ab3b522cc39750bb
yperezmartinez <yperezmartinez@eniac12.danielcastelao.org>
2015-01-26 09:25:56 +0100

    Commit sout Conflicto

--- src/boletin18/Boletin18.java
+++ src/boletin18/Boletin18.java
@@ -14,6 +14,7 @@ public class Boletin18 {
         for (int i = 0; i <= 5; i++) {
             numeros[i]=(int)(Math.random()*50);
             System.out.println(numeros[i]);
+            System.out.println("Prueba Conflicto");
             
         }
         System.out.println("***Al reves***");
